fa88ddce4dbf9084f38aacb9d9bc7ad25bef9684
Seiji Osada <seiji13r@outlook.com>
2018-11-30 23:07:20 -0600

    Initial Model Definition Completed User, Movie and UserMovie models Defined

---
+++ models/user.js
@@ -0,0 +1,20 @@
+// const bcrypt = require('bcryptjs');
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING },
+    name: { type: DataTypes.STRING },
+    salt: { type: DataTypes.STRING },
+    currentDay: { type: DataTypes.INTEGER, defaultValue: 1 }
+  });
+
+  User.associate = function(models) {
+    User.belongsToMany(models.Movie, {
+      through: models.UserMovie
+    });
+  };
+
+  return User;
+};
